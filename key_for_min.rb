24a809b8bf64ac2e091d7b51cc9774cbc7150a47
William Bugenis <wbugenis@gmail.com>
2020-12-11 00:10:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  lowest = 
   name_hash.each_with_index do |(key, value), index|
     
 end
