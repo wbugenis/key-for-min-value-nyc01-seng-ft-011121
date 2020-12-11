76529cba46cdd13a078dcba4e981c0107772c780
William Bugenis <wbugenis@gmail.com>
2020-12-11 00:22:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
     nil 
   end
   
-  lowest_num
-  lowest_key
+  lowest_num = 0
+  lowest_key = ""
   
   name_hash.each_with_index do |(key, value), index|
     if index == 0
